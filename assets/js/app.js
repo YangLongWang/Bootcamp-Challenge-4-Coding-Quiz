2857a9e8d8ac878ef9bbf592e2879a9615c5990d
yang <joey674901205@gmail.com>
2022-05-27 03:49:13 -0400

    feature/start

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,38 @@
-console.log("hello");
+
+
+
+function goViewHighScores() {
+    window.location.href = "high-scores.html";
+}
+
+function startQuiz() {
+    document.querySelector(".opening").style.display = "none";
+    var time = document.getElementById("time");
+    time.textContent = 60;
+    // Countdown one second jump
+
+
+    // Countdown one second jump end
+
+    document.scripts.href = "./questions.js";
+    var section = document.createElement("section");
+    section.className = "opening";
+    
+    // section.appendChild(questions);
+
+    console.log(section);
+    
+
+}
+
+
+
+
+
+
+
+
+document.getElementById("view-scores").addEventListener("click", goViewHighScores);    
+
+document.getElementById("btn-start").addEventListener("click", startQuiz);
+
