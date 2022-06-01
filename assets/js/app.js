8dd91e72206d44487f75ee96c90ea43b17809b89
yang <joey674901205@gmail.com>
2022-06-01 02:52:03 -0400

    can load all questions, can rightly show right and wrong and automaticly jump to next question, fixed some css

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,7 +41,7 @@ const questionsObj = [
     }
 ];
 
-var quizIdCounter = 0;
+var quizIdCounter = -1;
 var answerIdCounter = 0; 
 var quizIndex = 0;
 var scores = 0;
@@ -64,90 +64,121 @@ function startQuiz() {
 }
 
 function showQuiz() {
-    // whole one question (question + options)
-    var quiz = document.createElement("div");
-    quiz.className = "show-quiz";
-    quiz.setAttribute("data-quiz-id", quizIdCounter);
-    quiz.innerHTML = "<h1>" + questionsObj[quizIndex].question + "</h1>";
 
-    opening.appendChild(quiz);
+    if (quizIdCounter >= questionsObj.length) {
+        end();
 
-    // 4 options
-    var options = document.createElement("div");
-    options.className = "answer";
-    
-    quiz.appendChild(options);
+    } else {
 
-    var opA = document.createElement("button");
-    var opB = document.createElement("button");
-    var opC = document.createElement("button");
-    var opD = document.createElement("button");
+        // whole one question (question + options)
+        var quiz = document.createElement("div");
+        quiz.className = "show-quiz";
+        quiz.setAttribute("data-quiz-id", quizIdCounter);
+        quiz.innerHTML = "<h1>" + questionsObj[quizIndex].question + "</h1>";
 
-    opA.className = "btn btn-1";
-    opB.className = "btn btn-2";
-    opC.className = "btn btn-3";
-    opD.className = "btn btn-4";
+        opening.appendChild(quiz);
 
-    opA.setAttribute("data-answer-id", answerIdCounter);
-    opB.setAttribute("data-answer-id", answerIdCounter);
-    opC.setAttribute("data-answer-id", answerIdCounter);
-    opD.setAttribute("data-answer-id", answerIdCounter);
+        // 4 options
+        var options = document.createElement("div");
+        options.className = "answer";
+        
+        quiz.appendChild(options);
 
-    opA.textContent = questionsObj[quizIndex].a;
-    opB.textContent = questionsObj[quizIndex].b;
-    opC.textContent = questionsObj[quizIndex].c;
-    opD.textContent = questionsObj[quizIndex].d;
+        var opA = document.createElement("button");
+        var opB = document.createElement("button");
+        var opC = document.createElement("button");
+        var opD = document.createElement("button");
+
+        opA.className = "btn btn-1";
+        opB.className = "btn btn-2";
+        opC.className = "btn btn-3";
+        opD.className = "btn btn-4";
+
+        opA.setAttribute("data-answer-id", answerIdCounter);
+        opB.setAttribute("data-answer-id", answerIdCounter);
+        opC.setAttribute("data-answer-id", answerIdCounter);
+        opD.setAttribute("data-answer-id", answerIdCounter);
+
+        opA.textContent = questionsObj[quizIndex].a;
+        opB.textContent = questionsObj[quizIndex].b;
+        opC.textContent = questionsObj[quizIndex].c;
+        opD.textContent = questionsObj[quizIndex].d;
 
 
 
-    options.appendChild(opA);
-    options.appendChild(opB);
-    options.appendChild(opC);
-    options.appendChild(opD);
+        options.appendChild(opA);
+        options.appendChild(opB);
+        options.appendChild(opC);
+        options.appendChild(opD);
 
-    // 4 options end
-    // whole one question end
-    console.log(quiz);
-    quizIdCounter++;
-    answerIdCounter++;
-    quizIndex++;
+        // 4 options end
+        // whole one question end
+        console.log(quiz);
+        quizIdCounter++;
+        answerIdCounter++;
+        quizIndex++;
+        console.log(quizIdCounter);
 
-    // check the result
-    checkAnswer();
+        // check the result
+        checkAnswer();
+    }
 }
 
 function checkAnswer() {
-
     document.querySelector(".answer").addEventListener("click", function(event) {
         var targetEl = event.target;    
-
         if (targetEl.matches(".btn-4")) {
-            var check = document.getElementById("check");
-            check = document.createElement("p");
-            check.className = "check";
-            check.textContent = "Correct!";
-            check.setAttribute("style","font-size: 25px; font-style: italic; color: rgba(128, 128, 128, 1); letter-spacing: 1px; ")
-            result.appendChild(check);
+            result.innerHTML = "Correct!"
             scores = scores + 10;
             saveScores.push(scores);
-            console.log(scores);
             saveMark();
 
+            setTimeout(function() {
+                var next = document.querySelector(".show-quiz");
+                next.remove();
+                console.log(result);
+                result.innerHTML = "";         
+                showQuiz();                
+            }, 1000);
+
+            
         } else {
-            var check = document.getElementById("check");
-            check = document.createElement("p");
-            check.className = "check";
-            check.textContent = "Wrong!";
-            check.setAttribute("style","font-size: 25px; font-style: italic; color: rgba(128, 128, 128, 1); letter-spacing: 1px; ")
-            result.appendChild(check);
+            // var check = document.getElementById("check");
+            // check = document.createElement("p");
+            // check.className = "check";
+            // check.textContent = "Wrong!";
+            // check.setAttribute("style","font-size: 25px; font-style: italic; color: rgba(128, 128, 128, 1); letter-spacing: 1px; ")
+            // result.appendChild(check);
+            // scores = scores - 10;
+            // saveScores.push(scores);
+            // saveMark();
+            // console.log(result);
+            // showQuiz();
+
+            result.innerHTML = "Wrong!"
             scores = scores - 10;
             saveScores.push(scores);
-            console.log(scores);
             saveMark();
+
+            setTimeout(function() {
+                var next = document.querySelector(".show-quiz");
+                next.remove();
+                result.innerHTML = "";         
+                showQuiz();                
+            }, 1000);
         }
-
     })
+}
 
+function reset() {
+    if (quizIdCounter <= questionsObj.length) {
+        var next = document.querySelector(".show-quiz");
+        next.remove();
+        var reResult = document.querySelector(".check");
+        reResult.remove();                
+
+        showQuiz();
+    }
 }
 
 function countDown() {
@@ -160,7 +191,6 @@ function countDown() {
             timeEl.textContent = 0 + "s";
             clearInterval(timeInterval);
             end();
-            // show message (next?)
         }
     }, 1000);
 }
