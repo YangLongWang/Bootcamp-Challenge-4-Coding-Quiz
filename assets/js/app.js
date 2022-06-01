d4b3c1c8c0b1fd9ba7145ecbe92511032f01a6b1
yang <joey674901205@gmail.com>
2022-05-31 23:57:53 -0400

    add first question, can show right or wrong answer, countdown timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,53 +3,53 @@ const questionsObj = [
         question: "Commonly used data types DO NOT include:",
         a: "A. strings", 
         b: "B. booleans", 
-        c: "C. alerts", 
-        d: "D. numbers",
-        answer: "C. alerts"
+        c: "C. numbers", 
+        d: "D. alerts",
+        answer: "D. alerts"
     },
     {
         question: "The condition in an if/else statement is enclosed with _____.",
-        a: "A. quotes",
-        b: "B. curly brackets",
-        c: "C. parenthesis",
-        d: "D. square brackets",
-        answer: "C. parenthesis"
+        a: "A. quotes", 
+        b: "B. curly brackets", 
+        c: "C. square brackets", 
+        d: "D. parenthesis",
+        answer: "D. parenthesis"
     },
     {
         question: "Arrays in JavaScript can be used to store _____.",
-        a: "A. numbers and strings",
-        b: "B. other arrays",
-        c: "C. booleans",
+        a: "A. numbers and strings", 
+        b: "B. other arrays", 
+        c: "C. booleans", 
         d: "D. all of the above",
         answer: "D. all of the above"
     },
     {
         question: "String values must be enclosed within _____ when being assigned to variables.",
-        a: "A. commas",
-        b: "B. curly brackets",
-        c: "C. quotes",
-        d: "D. parenthesis",
-        answer: "C. quotes"
+        a: "A. commas", 
+        b: "B. curly brackets", 
+        c: "C. parenthesis", 
+        d: "D. quotes",
+        answer: "D. quotes"
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        a: "A. JavaScript",
-        b: "B. terminal/bash",
-        c: "C. for loops",
-        d: "D. console.log",
+        a: "A. JavaScript", 
+        b: "B. terminal/bash", 
+        c: "C. for loops", 
+        d:"D. console.log",
         answer: "D. console.log"
     }
 ];
 
-// const rightAnswer = ["c", "c", "d", "c", "d"];
-
-var optionIdCounter = 0;
+var quizIdCounter = 0;
+var answerIdCounter = 0; 
+var quizIndex = 0;
+var scores = 0;
+var timeLeft = 0;
 var opening = document.querySelector(".opening");
 var result = document.querySelector(".result");
 var timeEl = document.getElementById("time");
-
-// var timeLeft = 75;
-
+var saveScores = [];
 
 
 function goViewHighScores() {
@@ -65,32 +65,40 @@ function startQuiz() {
 
 function showQuiz() {
     // whole one question (question + options)
-    var quiz = document.createElement("section");
+    var quiz = document.createElement("div");
     quiz.className = "show-quiz";
-    quiz.innerHTML = "<h1>" + questionsObj[0].question + "</h1>";
+    quiz.setAttribute("data-quiz-id", quizIdCounter);
+    quiz.innerHTML = "<h1>" + questionsObj[quizIndex].question + "</h1>";
 
     opening.appendChild(quiz);
 
     // 4 options
-    var options = document.createElement("section");
+    var options = document.createElement("div");
     options.className = "answer";
-    options.setAttribute("data-options-id", optionIdCounter);
+    
     quiz.appendChild(options);
 
-    var opA = document.createElement("option");
-    var opB = document.createElement("option");
-    var opC = document.createElement("option");
-    var opD = document.createElement("option");
+    var opA = document.createElement("button");
+    var opB = document.createElement("button");
+    var opC = document.createElement("button");
+    var opD = document.createElement("button");
 
-    opA.className = "btn";
-    opB.className = "btn";
-    opC.className = "btn";
-    opD.className = "btn";
+    opA.className = "btn btn-1";
+    opB.className = "btn btn-2";
+    opC.className = "btn btn-3";
+    opD.className = "btn btn-4";
 
-    opA.textContent = questionsObj[0].a;
-    opB.textContent = questionsObj[0].b;
-    opC.textContent = questionsObj[0].c;
-    opD.textContent = questionsObj[0].d;
+    opA.setAttribute("data-answer-id", answerIdCounter);
+    opB.setAttribute("data-answer-id", answerIdCounter);
+    opC.setAttribute("data-answer-id", answerIdCounter);
+    opD.setAttribute("data-answer-id", answerIdCounter);
+
+    opA.textContent = questionsObj[quizIndex].a;
+    opB.textContent = questionsObj[quizIndex].b;
+    opC.textContent = questionsObj[quizIndex].c;
+    opD.textContent = questionsObj[quizIndex].d;
+
+
 
     options.appendChild(opA);
     options.appendChild(opB);
@@ -100,41 +108,50 @@ function showQuiz() {
     // 4 options end
     // whole one question end
     console.log(quiz);
-    optionIdCounter++;
-    console.log(options);
-    // check the result
+    quizIdCounter++;
+    answerIdCounter++;
+    quizIndex++;
 
-    var checkAnswer = document.querySelector(".answer").addEventListener("click", checkAnswer);
+    // check the result
+    checkAnswer();
 }
 
-function checkAnswer(event) {
-    var answerEl = event.target;
+function checkAnswer() {
 
-    if (answerEl.matches("questionsObj".answer)) {
-        var check = document.getElementById("check");
-        check = document.createElement("p");
-        check.className = "check";
-        check.textContent = "Correct!";
-        check.setAttribute("style","font-size: 25px; font-style: italic; color: rgba(128, 128, 128, 1); letter-spacing: 1px; ")
-        result.appendChild(check);
+    document.querySelector(".answer").addEventListener("click", function(event) {
+        var targetEl = event.target;    
 
-        console.log(result);
+        if (targetEl.matches(".btn-4")) {
+            var check = document.getElementById("check");
+            check = document.createElement("p");
+            check.className = "check";
+            check.textContent = "Correct!";
+            check.setAttribute("style","font-size: 25px; font-style: italic; color: rgba(128, 128, 128, 1); letter-spacing: 1px; ")
+            result.appendChild(check);
+            scores = scores + 10;
+            saveScores.push(scores);
+            console.log(scores);
+            saveMark();
 
-    } else {
-        var check = document.getElementById("check");
-        check = document.createElement("p");
-        check.className = "check";
-        check.textContent = "Wrong!";
-        check.setAttribute("style","font-size: 25px; font-style: italic; color: rgba(128, 128, 128, 1); letter-spacing: 1px; ")
-        result.appendChild(check);
+        } else {
+            var check = document.getElementById("check");
+            check = document.createElement("p");
+            check.className = "check";
+            check.textContent = "Wrong!";
+            check.setAttribute("style","font-size: 25px; font-style: italic; color: rgba(128, 128, 128, 1); letter-spacing: 1px; ")
+            result.appendChild(check);
+            scores = scores - 10;
+            saveScores.push(scores);
+            console.log(scores);
+            saveMark();
+        }
 
-        console.log(result);
+    })
 
-    }
 }
 
 function countDown() {
-    var timeLeft = 75;
+    timeLeft = 75;
     var timeInterval = setInterval(function() {
         if (timeLeft >= 1) {
             timeEl.textContent = timeLeft + "s";
@@ -142,11 +159,20 @@ function countDown() {
         } else {
             timeEl.textContent = 0 + "s";
             clearInterval(timeInterval);
+            end();
             // show message (next?)
         }
     }, 1000);
 }
 
+function end() {
+    console.log("end, show high scores and record name");
+}
+
+var saveMark = function() {
+    localStorage.setItem("scores", JSON.stringify(saveScores));
+    console.log(saveScores);
+}
 
 
 document.getElementById("view-scores").addEventListener("click", goViewHighScores);    
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,58 +1,180 @@
 const questionsObj = [
     {
-        question: "Commonly used data types DO NOT include:", 
+        question: "Commonly used data types DO NOT include:",
         a: "A. strings", 
         b: "B. booleans", 
-        c: "C. alerts", 
-        d: "D. numbers"
+        c: "C. numbers", 
+        d: "D. alerts",
+        answer: "D. alerts"
     },
     {
         question: "The condition in an if/else statement is enclosed with _____.",
-        a: "A. quotes",
-        b: "B. curly brackets",
-        c: "C. parenthesis",
-        d: "D. square brackets"
+        a: "A. quotes", 
+        b: "B. curly brackets", 
+        c: "C. square brackets", 
+        d: "D. parenthesis",
+        answer: "D. parenthesis"
     },
     {
         question: "Arrays in JavaScript can be used to store _____.",
-        a: "A. numbers and strings",
-        b: "B. other arrays",
-        c: "C. booleans",
-        d: "D. all of the above"
+        a: "A. numbers and strings", 
+        b: "B. other arrays", 
+        c: "C. booleans", 
+        d: "D. all of the above",
+        answer: "D. all of the above"
     },
     {
         question: "String values must be enclosed within _____ when being assigned to variables.",
-        a: "A. commas",
-        b: "B. curly brackets",
-        c: "C. quotes",
-        d: "D. parenthesis"
+        a: "A. commas", 
+        b: "B. curly brackets", 
+        c: "C. parenthesis", 
+        d: "D. quotes",
+        answer: "D. quotes"
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        a: "A. JavaScript",
-        b: "B. terminal/bash",
-        c: "C. for loops",
-        d: "D. console.log"
-    },
-    {
-        question: "",
-        a: "A. ",
-        b: "B. ",
-        c: "C. ",
-        d: "D. "
+        a: "A. JavaScript", 
+        b: "B. terminal/bash", 
+        c: "C. for loops", 
+        d:"D. console.log",
+        answer: "D. console.log"
     }
 ];
 
-var callBack = function(eventType, fn) {
-    e = {
-        name: "Longyang"
-    }
-    if( eventType === "click") {
-        fn(e)
-    }
+var quizIdCounter = 0;
+var answerIdCounter = 0; 
+var quizIndex = 0;
+var scores = 0;
+var timeLeft = 0;
+var opening = document.querySelector(".opening");
+var result = document.querySelector(".result");
+var timeEl = document.getElementById("time");
+var saveScores = [];
+
+
+function goViewHighScores() {
+    window.location.href = "high-scores.html";
 }
 
-callBack("click" , function(e) {
-    console.log(e.name);
-})
+function startQuiz() {
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".divider").style.visibility = "visible";
 
+    showQuiz();
+}
+
+function showQuiz() {
+    // whole one question (question + options)
+    var quiz = document.createElement("div");
+    quiz.className = "show-quiz";
+    quiz.setAttribute("data-quiz-id", quizIdCounter);
+    quiz.innerHTML = "<h1>" + questionsObj[quizIndex].question + "</h1>";
+
+    opening.appendChild(quiz);
+
+    // 4 options
+    var options = document.createElement("div");
+    options.className = "answer";
+    
+    quiz.appendChild(options);
+
+    var opA = document.createElement("button");
+    var opB = document.createElement("button");
+    var opC = document.createElement("button");
+    var opD = document.createElement("button");
+
+    opA.className = "btn btn-1";
+    opB.className = "btn btn-2";
+    opC.className = "btn btn-3";
+    opD.className = "btn btn-4";
+
+    opA.setAttribute("data-answer-id", answerIdCounter);
+    opB.setAttribute("data-answer-id", answerIdCounter);
+    opC.setAttribute("data-answer-id", answerIdCounter);
+    opD.setAttribute("data-answer-id", answerIdCounter);
+
+    opA.textContent = questionsObj[quizIndex].a;
+    opB.textContent = questionsObj[quizIndex].b;
+    opC.textContent = questionsObj[quizIndex].c;
+    opD.textContent = questionsObj[quizIndex].d;
+
+
+
+    options.appendChild(opA);
+    options.appendChild(opB);
+    options.appendChild(opC);
+    options.appendChild(opD);
+
+    // 4 options end
+    // whole one question end
+    console.log(quiz);
+    quizIdCounter++;
+    answerIdCounter++;
+    quizIndex++;
+
+    // check the result
+    checkAnswer();
+}
+
+function checkAnswer() {
+
+    document.querySelector(".answer").addEventListener("click", function(event) {
+        var targetEl = event.target;    
+
+        if (targetEl.matches(".btn-4")) {
+            var check = document.getElementById("check");
+            check = document.createElement("p");
+            check.className = "check";
+            check.textContent = "Correct!";
+            check.setAttribute("style","font-size: 25px; font-style: italic; color: rgba(128, 128, 128, 1); letter-spacing: 1px; ")
+            result.appendChild(check);
+            scores = scores + 10;
+            saveScores.push(scores);
+            console.log(scores);
+            saveMark();
+
+        } else {
+            var check = document.getElementById("check");
+            check = document.createElement("p");
+            check.className = "check";
+            check.textContent = "Wrong!";
+            check.setAttribute("style","font-size: 25px; font-style: italic; color: rgba(128, 128, 128, 1); letter-spacing: 1px; ")
+            result.appendChild(check);
+            scores = scores - 10;
+            saveScores.push(scores);
+            console.log(scores);
+            saveMark();
+        }
+
+    })
+
+}
+
+function countDown() {
+    timeLeft = 75;
+    var timeInterval = setInterval(function() {
+        if (timeLeft >= 1) {
+            timeEl.textContent = timeLeft + "s";
+            timeLeft--;
+        } else {
+            timeEl.textContent = 0 + "s";
+            clearInterval(timeInterval);
+            end();
+            // show message (next?)
+        }
+    }, 1000);
+}
+
+function end() {
+    console.log("end, show high scores and record name");
+}
+
+var saveMark = function() {
+    localStorage.setItem("scores", JSON.stringify(saveScores));
+    console.log(saveScores);
+}
+
+
+document.getElementById("view-scores").addEventListener("click", goViewHighScores);    
+document.getElementById("btn-start").addEventListener("click", startQuiz);
+document.getElementById("btn-start").addEventListener("click", countDown);
